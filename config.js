0166bf8224e566ff7a25d7f0df0a1e33677ee4fb
vodinhhoang <92923900+vodinhhoang@users.noreply.github.com>
2021-10-21 21:16:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Hải ánh';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc Ánh của tui 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với m trong thế giới nhỏ bé này ';
+const contentCard = 'Chúc Ánh của tui 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với m trong thế giới nhỏ bé này.Anh sẽ luônd đứng sau để che chở cho em bảo vệ em Love You ';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
