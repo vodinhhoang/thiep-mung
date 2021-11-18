19b44fae836d836f9ec4bfc8c0501e14c8345e0d
vodinhhoang <92923900+vodinhhoang@users.noreply.github.com>
2021-11-18 07:57:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const contentCard = 'Chúc Ánh của t sinh nhật vui vẻ  tràn ngập niề
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png.Jng';
+const giftImage = 'hot-girl.png.jpg';
 const base64 = '';
-const giftImageBase64 = "data:image/png;base64, " + base64;
+const giftImageBase64 = "data:image/Jpg;base64, " + base64;
