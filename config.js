c01152fd1a1dff7e2744d5f370ecf2a5414935e2
vodinhhoang <92923900+vodinhhoang@users.noreply.github.com>
2022-02-21 14:56:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Chị Thương';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng sinh nhật ';
 const titleCard = 'Tặng cho chị';
-const contentCard = 'Chúc Chị của tui sinh nhật vui vẻ  tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với chị trong thế giới nhỏ bé này. Chúc All nhó Không có gì chỉ có Hoàng ở đây chúc sinh nhật chị thôi:)) ';
+const contentCard = 'Chúc Chị của tui sinh nhật vui vẻ  tràn ngập niềm vui và những nụ cười.Mong điều đẹp nhất sẽ đến với chị trong thế giới nhỏ bé này. Chúc All nhó Không có gì chỉ có Hoàng ở đây chúc sinh nhật chị thôi:)) ';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
